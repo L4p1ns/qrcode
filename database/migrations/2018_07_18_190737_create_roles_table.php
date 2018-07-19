af2d64609a2dd1820315ce86c552c1f30c911abc
bayendemba <bayendemba@gmail.com>
2018-07-19 13:11:18 +0000

    implement softDeletes()

--- database/migrations/2018_07_18_190737_create_roles_table.php
+++ database/migrations/2018_07_18_190737_create_roles_table.php
@@ -16,6 +16,7 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');// Admin, Moderators, Webmasters, Buyers
+            $table->softDeletes();
             $table->timestamps();
         });
     }
