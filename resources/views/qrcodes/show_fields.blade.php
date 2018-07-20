8a8928a7bc5db3794963ca1d383ede1f10b64504
bayendemba <bayendemba@gmail.com>
2018-07-20 15:58:34 +0000

    Clean qrcode views

--- resources/views/qrcodes/show_fields.blade.php
+++ resources/views/qrcodes/show_fields.blade.php
@@ -1,73 +1,68 @@
-<!-- Id Field -->
-<div class="form-group">
-    {!! Form::label('id', 'Id:') !!}
-    <p>{!! $qrcode->id !!}</p>
-</div>
+<div class="col-md-6">
 
-<!-- User Id Field -->
-<div class="form-group">
-    {!! Form::label('user_id', 'User Id:') !!}
-    <p>{!! $qrcode->user_id !!}</p>
-</div>
+    <!-- Product Name Field -->
+    <div class="form-group">
+        <h3>
+            {!! $qrcode->product_name !!}
+            <br /> @if(isset($qrcode->company_name))
+            <small>By {!! $qrcode->company_name !!}</small> @endif
+        </h3>
+    </div>
 
-<!-- Website Field -->
-<div class="form-group">
-    {!! Form::label('website', 'Website:') !!}
-    <p>{!! $qrcode->website !!}</p>
-</div>
+    <!-- User Id Field -->
+    <div class="form-group">
+        {!! Form::label('user_id', 'User Name:') !!}
+        <p>{!! $qrcode->user_id !!}</p>
+    </div>
 
-<!-- Company Name Field -->
-<div class="form-group">
-    {!! Form::label('company_name', 'Company Name:') !!}
-    <p>{!! $qrcode->company_name !!}</p>
-</div>
+    <!-- Website Field -->
+    <div class="form-group">
+        {!! Form::label('website', 'Website:') !!}
+        <p>{!! $qrcode->website !!}</p>
+    </div>
 
-<!-- Product Name Field -->
-<div class="form-group">
-    {!! Form::label('product_name', 'Product Name:') !!}
-    <p>{!! $qrcode->product_name !!}</p>
-</div>
+    <!-- Product Url Field -->
+    <div class="form-group">
+        {!! Form::label('product_url', 'Product Url:') !!}
+        <p>{!! $qrcode->product_url !!}</p>
+    </div>
 
-<!-- Product Url Field -->
-<div class="form-group">
-    {!! Form::label('product_url', 'Product Url:') !!}
-    <p>{!! $qrcode->product_url !!}</p>
-</div>
+    <!-- Callback Url Field -->
+    <div class="form-group">
+        {!! Form::label('callback_url', 'Callback Url:') !!}
+        <p>{!! $qrcode->callback_url !!}</p>
+    </div>
 
-<!-- Callback Url Field -->
-<div class="form-group">
-    {!! Form::label('callback_url', 'Callback Url:') !!}
-    <p>{!! $qrcode->callback_url !!}</p>
-</div>
+    <!-- Amount Field -->
+    <div class="form-group">
+        {!! Form::label('amount', 'Amount:') !!}
+        <p>{!! $qrcode->amount !!}</p>
+    </div>
 
-<!-- Qrcode Path Field -->
-<div class="form-group">
-    {!! Form::label('qrcode_path', 'Qrcode Path:') !!}
-    <p>{!! $qrcode->qrcode_path !!}
-        <img src="{{asset($qrcode->qrcode_path)}}">
-    </p>
-</div>
+    <!-- Status Field -->
+    <div class="form-group">
+        {!! Form::label('status', 'Status:') !!}
+        <p>{!! $qrcode->status !!}</p>
+    </div>
 
-<!-- Amount Field -->
-<div class="form-group">
-    {!! Form::label('amount', 'Amount:') !!}
-    <p>{!! $qrcode->amount !!}</p>
-</div>
+    <!-- Created At Field -->
+    <div class="form-group">
+        {!! Form::label('created_at', 'Created At:') !!}
+        <p>{!! $qrcode->created_at !!}</p>
+    </div>
 
-<!-- Status Field -->
-<div class="form-group">
-    {!! Form::label('status', 'Status:') !!}
-    <p>{!! $qrcode->status !!}</p>
+    <!-- Updated At Field -->
+    <div class="form-group">
+        {!! Form::label('updated_at', 'Updated At:') !!}
+        <p>{!! $qrcode->updated_at !!}</p>
+    </div>
 </div>
-
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{!! $qrcode->created_at !!}</p>
-</div>
-
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{!! $qrcode->updated_at !!}</p>
+<div class="col-md-5 pull-right">
+    <!-- Qrcode Path Field -->
+    <div class="form-group">
+        {!! Form::label('qrcode_path', 'Scan Qrcode and Pay with Our App:') !!}
+        <p>
+            <img src="{{asset($qrcode->qrcode_path)}}">
+        </p>
+    </div>
 </div>
