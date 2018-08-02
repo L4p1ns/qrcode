f51f962e5ea7661b5da275ae4d61e11a8f5c1dd6
bayendemba <bayendemba@gmail.com>
2018-08-02 19:28:55 +0000

    User Roles and Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Flash;
+use Response;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
 use App\Http\Requests\CreateUserRequest;
 use App\Http\Requests\UpdateUserRequest;
-use App\Repositories\UserRepository;
 use App\Http\Controllers\AppBaseController;
-use Illuminate\Http\Request;
-use Flash;
 use Prettus\Repository\Criteria\RequestCriteria;
-use Response;
 
 class UserController extends AppBaseController
 {
@@ -100,8 +101,11 @@ class UserController extends AppBaseController
 
             return redirect(route('users.index'));
         }
+        $roles = Role::all();
 
-        return view('users.edit')->with('user', $user);
+        return view('users.edit')
+                ->with('user', $user)
+                ->with('roles', $roles);
     }
 
     /**
--- resources/views/users/fields.blade.php
+++ resources/views/users/fields.blade.php
@@ -1,35 +1,36 @@
 <!-- Name Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('name', 'Name:') !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    {!! Form::label('name', 'Name:') !!} {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Role Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('role_id', 'Role Id:') !!}
-    {!! Form::number('role_id', null, ['class' => 'form-control']) !!}
-</div>
+@if(Auth::user()->role_id
+< 3) <!-- Role Id Field -->
+    <div class="form-group col-sm-6">
+        {!! Form::label('role_id', 'User level:') !!} {!! Form::number('role_id', null, ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Email Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('email', 'Email:') !!}
-    {!! Form::email('email', null, ['class' => 'form-control']) !!}
-</div>
+    <div class="form-group col-sm-6">
+        <label for="sel1">User level:</label>
+        <select class="form-control" id="sel1">
+            <option value="{{ $user->role['id'] }}">{{ $user->role['name'] }}</option>            
+                @foreach($roles as $role)
+                    <option value="{{ $role['id'] }}">{{ $role['name'] }}</option>
+                @endforeach
+            </select>
+    </div>
+    @endif
+    <!-- Email Field -->
+    <div class="form-group col-sm-6">
+        {!! Form::label('email', 'Email:') !!} {!! Form::email('email', null, ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Password Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('password', 'Password:') !!}
-    {!! Form::password('password', ['class' => 'form-control']) !!}
-</div>
+    <!-- Password Field -->
+    <div class="form-group col-sm-6">
+        {!! Form::label('password', 'Password:') !!} {!! Form::password('password', ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Remember Token Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('remember_token', 'Remember Token:') !!}
-    {!! Form::text('remember_token', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('users.index') !!}" class="btn btn-default">Cancel</a>
-</div>
+    <!-- Submit Field -->
+    <div class="form-group col-sm-12">
+        {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+        <a href="{!! route('users.index') !!}" class="btn btn-default">Cancel</a>
+    </div>
